10193a37e0fe83723c76d90033c0ab6e0a1c4020
NorbertAll <norbert_gutkowski@interia.pl>
2022-12-07 23:01:34 +0100

    draw circle

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ WIDTH, HEIGHT = 800, 500
 win=pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman Game!")
 
+RADIUS=20
+GAP=15
+letters=[]
+startx=round((WIDTH - (RADIUS *2+GAP)*13)/2)
+starty=400
+for i in range(26):
+    x= startx + GAP*2 +(RADIUS*2 + GAP)*(i%13)
+    y= starty+((i//13) *(GAP+RADIUS*2))
+    letters.append([x,y])
+
 images=[]
 for i in range(7):
     image=pygame.image.load("hangman"+str(i)+".jpg")
@@ -17,11 +27,21 @@ FPS = 60
 clock= pygame.time.Clock()
 run = True
 
-while run:
-    clock.tick(FPS)
+def draw():
     win.fill((255, 255, 255))
+
+    for letter in letters:
+        x, y=letter
+        pygame.draw.circle(win, (0, 0, 0), (x, y), RADIUS, 3)
+
+
+
     win.blit(images[hangman_status], (0,0))
     pygame.display.update()
+
+while run:
+    clock.tick(FPS)
+    draw()
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
             run=False
