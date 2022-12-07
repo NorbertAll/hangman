6c1a8403cd5a46637f5559246f8828507bb7788f
NorbertAll <norbert_gutkowski@interia.pl>
2022-12-07 21:13:10 +0100

    display images and fix name images

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ WIDTH, HEIGHT = 800, 500
 win=pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman Game!")
 
+images=[]
+for i in range(7):
+    image=pygame.image.load("hangman"+str(i)+".jpg")
+    images.append(image)
+
+hangman_status= 0
+
 FPS = 60
 clock= pygame.time.Clock()
 run = True
 
 while run:
     clock.tick(FPS)
-
+    win.fill((255, 255, 255))
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
             run=False
