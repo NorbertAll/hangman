9792e7d9bf0cb66d28cd16ec6ebd2c00b6efb6ef
NorbertAll <norbert_gutkowski@interia.pl>
2022-12-07 23:53:12 +0100

    draw letters in circle

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ GAP=15
 letters=[]
 startx=round((WIDTH - (RADIUS *2+GAP)*13)/2)
 starty=400
+A=65
 for i in range(26):
     x= startx + GAP*2 +(RADIUS*2 + GAP)*(i%13)
     y= starty+((i//13) *(GAP+RADIUS*2))
-    letters.append([x,y])
+    letters.append([x,y, chr(A+i)])
 
+LETTER_FONT=pygame.font.SysFont('comicsans', 32)
 images=[]
 for i in range(7):
     image=pygame.image.load("hangman"+str(i)+".png")
@@ -31,9 +33,10 @@ def draw():
     win.fill((255, 255, 255))
 
     for letter in letters:
-        x, y=letter
+        x, y, ltr=letter
         pygame.draw.circle(win, (0, 0, 0), (x, y), RADIUS, 3)
-
+        text= LETTER_FONT.render(ltr, 1 , (0, 0, 0))
+        win.blit(text, (x-text.get_width()/2, y-text.get_height()/2))
 
 
     win.blit(images[hangman_status], (0,0))
